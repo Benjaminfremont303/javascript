951ad0e297ba3d20e3310ffac2de32a1224f9c99
benjaminfremont303 <fremont.benjamin303@gmail.com>
2023-03-05 20:16:58 +0100

    player fin

--- gw/gw.js
+++ gw/gw.js
@@ -1,10 +1,37 @@
 let screen = document.querySelector(".screen");
-let player = document.querySelector(".player");
+let playerc = document.querySelector(".playercentre");
+let playerg = document.querySelector(".playergauche");
+let playerd = document.querySelector(".playerdroite");
 
 
-compt.addEventListener("click",function(){
-    nbr++;
-    p1.innerText = nbr;
+
+playerc.addEventListener('click', function() {
+ 
+        playerc.style.display="0";
+        playerg.style.display="1";        
+        // cookie.classList.toggle("divchange");
 
     
-});
+  });
+
+  document.addEventListener('keydown', (e) => {
+   if(e.key == "ArrowLeft"){
+    //    alert(`first keydown event. key property value is "${e.key}"`);
+    playerd.style.display="none";  
+    playerc.style.display="none";
+    playerg.style.display="block";  
+}
+if (e.key == "ArrowRight"){
+
+    playerg.style.display="none";  
+    playerc.style.display="none";
+    playerd.style.display="block";  
+}
+  });
+
+  document.addEventListener('keyup', (e) => {
+
+        playerg.style.display="none";  
+        playerc.style.display="block";
+        playerd.style.display="none";  
+  });
