960cf5bb19477396c253821fbaa5b9c566763c42
benjaminfremont303 <fremont.benjamin303@gmail.com>
2023-03-05 23:06:04 +0100

    collision ?

--- gw/gw.js
+++ gw/gw.js
@@ -2,13 +2,17 @@ let screen = document.querySelector(".screen");
 let playerc = document.querySelector(".playercentre");
 let playerg = document.querySelector(".playergauche");
 let playerd = document.querySelector(".playerdroite");
+let drop5 = document.querySelector(".drop5");
+let splash1 = document.querySelector(".splash1");
+
+
 
 
 
 playerc.addEventListener('click', function() {
  
-        playerc.style.display="0";
-        playerg.style.display="1";        
+        playerc.style.visibility="hidden";
+        playerg.style.visibility="visible";        
         // cookie.classList.toggle("divchange");
 
     
@@ -17,21 +21,27 @@ playerc.addEventListener('click', function() {
   document.addEventListener('keydown', (e) => {
    if(e.key == "ArrowLeft"){
     //    alert(`first keydown event. key property value is "${e.key}"`);
-    playerd.style.display="none";  
-    playerc.style.display="none";
-    playerg.style.display="block";  
+    playerd.style.visibility="hidden";  
+    playerc.style.visibility="hidden";
+    playerg.style.visibility="visible";  
 }
 if (e.key == "ArrowRight"){
 
-    playerg.style.display="none";  
-    playerc.style.display="none";
-    playerd.style.display="block";  
+    playerg.style.visibility="hidden";  
+    playerc.style.visibility="hidden";
+    playerd.style.visibility="visible";  
 }
   });
 
   document.addEventListener('keyup', (e) => {
 
-        playerg.style.display="none";  
-        playerc.style.display="block";
-        playerd.style.display="none";  
+        playerg.style.visibility="hidden";  
+        playerc.style.visibility="visible";
+        playerd.style.visibility="hidden";  
   });
+ document.addEventListener("style", function{
+    if (playerg.style.visibility == "hidden" && drop5.style.visibility == "visible"){
+        alert('perdu');
+        splash1.style.visibility = "visible";        
+    }});
+
