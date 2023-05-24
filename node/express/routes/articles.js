07c5af8f0af067da31f64a57919746c72f5fe1a0
Benjaminfremont303 <124568438+Benjaminfremont303@users.noreply.github.com>
2023-05-24 12:30:35 +0200

    hhh

--- node/express/routes/articles.js
+++ node/express/routes/articles.js
@@ -12,24 +12,13 @@ router.get('/articles', async (req, res) => {
   const articles = await Article.find().sort({ createdAt: 'desc' });
   res.render('index', { articles });
 });
-
 // Formulaire de création d'article
  router.get('/create', (req, res) => {
   res.render('create');
 }); 
 
-// Créer un nouvel article   
-router.post('/articles/create', async (req, res) => {
-  const { title, content } = req.body;
-  const article = new Article({ title, content });
-  await article.save();
-  res.redirect('/articles');
-});
-
 // Afficher un article
-router.get('/:id', async (req, response) => {
- /*  const article = await Article.findById(req.params.id.trim()).exec;
-  res.render('articles/show', { article }); */
+router.get('/:id', async (req, response) => {       
   try {
     const id = req.params.id.trim();
     if (!mongoose.Types.ObjectId.isValid(id)) throw new Error('invalid id') // validating `id`
@@ -48,40 +37,32 @@ router.get('/:id/edit', async (req, res) => {
   const article = await Article.findById(req.params.id);
   res.render('edit', { article });
 });
-// Mettre à jour l'article
-/* router.put('/:id', async (req, res) => {
-  const articleId = req.params.id.trim();
- res.write(articleId);
-  const updatedArticle = {
-    title: req.body.title,
-    content: req.body.content
-  }; */
   router.post('/:id', async (req, res) => {
     const articleId = req.params.id.trim();
     const updatedArticle = {
       title: req.body.title,
       content: req.body.content
     };
-  
     if (req.body._method === 'PUT') {
       // Effectuez la mise à jour de l'article ici
       try {
         const updated = await Article.findOneAndUpdate({ _id: articleId }, updatedArticle, { new: true });
-        console.log('ehejhej');
+        res.redirect('/articles/'+articleId);
       } catch (err) {
-        console.log(articleId);
         console.log(err);
       }
     } else {
-      // Autres actions de routage si nécessaire
-    }
+      const { title, content } = req.body;
+      console.log(req.body);
+      const article = new Article({ title, content });
+      await article.save()
+      res.redirect('/articles');    }
   });
   
-
-/*  // Supprimer un article
+ // Supprimer un article
 router.delete('/:id', async (req, res) => {
   await Article.findByIdAndDelete(req.params.id);
   res.redirect('/articles');
-}); */
+}); 
  
 module.exports = router;
